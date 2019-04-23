9a32d24dead19ec1c881a2815165fffd2a2cac03
rudderbucky <anandkwork7@gmail.com>
2019-04-22 19:02:25 -0500

    Button/rectangle pair

--- application/Main.java
+++ application/Main.java
@@ -7,12 +7,41 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+	    
+	    class ButtonPair {
+	        
+	        private HBox box;
+	        private ButtonPair(String txt) {
+	            box = new HBox();
+	            box.getStyleClass().add("quiz-hbox");
+	            Text text = new Text(txt);
+	            StackPane pane = new StackPane();
+	            Rectangle rect = new Rectangle(26,26);
+	            pane.getChildren().addAll(rect, text);
+	            rect.getStyleClass().add("quiz-rect");
+	            box.getChildren().add(pane);
+	            Button but = new Button("Hello world!");
+	            but.setMinWidth(300);
+	            but.setStyle("-fx-background-color: cyan;");
+	            but.setOnAction((ActionEvent event) -> { 
+	                but.setStyle("-fx-background-color: lime;"
+	                        + "-fx-text-fill: white;");
+	                but.setText("Correct!");
+	            
+	            });
+	            box.getChildren().add(but);
+	        }
+	    }
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,1000,600);
@@ -21,17 +50,12 @@ public class Main extends Application {
 			Label quizNo = new Label("Question X/X");
 			quizNo.getStyleClass().add("quiz-text");
 			VBox buttonBox = new VBox();
-			Button but = new Button("Hello world!");
-			but.setMinWidth(300);
-			but.setStyle("-fx-background-color: cyan;");
-			but.setOnAction((ActionEvent event) -> { 
-			    but.setStyle("-fx-background-color: lime;"
-			            + "-fx-text-fill: white;");
-			    but.setText("Correct!");
 			
-			});
 			buttonBox.getStyleClass().add("quiz-vbox");
-			buttonBox.getChildren().add(but);
+			for(int i = 0; i < 4; i++) {
+			    buttonBox.getChildren().add(new ButtonPair("A").box);
+			}
+
 			root.setTop(quizNo);
 			root.setLeft(buttonBox);
 			primaryStage.setScene(scene);
