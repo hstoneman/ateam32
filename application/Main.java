a32fad59e8277fcff7f9097894454de89b257992
mayukhmisra <36494487+mayukhmisra@users.noreply.github.com>
2019-04-29 18:32:14 -0500

    Homepage and Quiz initialize

--- application/Main.java
+++ application/Main.java
@@ -2,12 +2,13 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import application.Main.ButtonPair;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -18,6 +19,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -124,14 +126,13 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
 
-    primaryStage.setScene(Homepage(primaryStage));
+    primaryStage.setScene(homepage(primaryStage));
     primaryStage.setTitle("Quiz Generator - A Team 32");
     primaryStage.show();
 
   }
 
-  @SuppressWarnings("unused")
-  private Scene Homepage(Stage primaryStage) throws Exception {
+  private Scene homepage(Stage primaryStage) throws Exception {
 
     // Declare BorderPane
     BorderPane border = new BorderPane();
@@ -216,8 +217,7 @@ public class Main extends Application {
     addQ.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event) {
-        // primaryStage.setScene(AddQuestion(primaryStage, qTopics));
-        // TODO Make methods to return the scene for other scenes too.
+        primaryStage.setScene(addQuestion());
       }
     });
 
@@ -266,11 +266,54 @@ public class Main extends Application {
     return homepage;
   }
 
+  private Scene addQuestion() {
 
-  // TODO Write method to create new scene/
-  // private Scene AddQuestion(Stage primaryStage, ArrayList<String> topics) throws Exception {
-  //
-  // }
+    GridPane grid = new GridPane();
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(0, 10, 0, 10));
+    VBox col1 = new VBox();
+    VBox col2 = new VBox();
+
+    // Labels
+    Label label1 = new Label("Adding to the Question Bank");
+    label1.setStyle("-fx-font-weight: bold");
+    Label label2 = new Label("Question text");
+    Label label3 = new Label("Topic");
+    Label label4 = new Label("Image if desired");
+    Label label5 = new Label("Question answer");
+    Label label6 = new Label("Question choices");
+
+    // Buttons
+    Button choice = new Button("ADD CHOICE");
+    Button submit = new Button("SUBMIT");
+    submit.setStyle("-fx-background-color: #00ff00");
+
+    // Text fields
+    TextField textField1 = new TextField("Question Text");
+    TextField textField2 = new TextField("Topic");
+    TextField textField3 = new TextField("File Path");
+    TextField textField4 = new TextField("Answer");
+    TextField textField5 = new TextField("Choice Text");
+
+    // vBox spacing and adding nodes
+    col1.setSpacing(20);
+    col2.setSpacing(10);
+    ObservableList<Node> list1 = col1.getChildren();
+    list1.addAll(label2, label3, label4, label5, label6);
+    ObservableList<Node> list2 = col2.getChildren();
+    list2.addAll(textField1, textField2, textField3, textField4, textField5, choice, submit);
+
+    // Grid pane setting
+    grid.add(label1, 0, 0);
+    grid.add(col1, 0, 1);
+    grid.add(col2, 1, 1);
+
+    // Scene setting
+    Scene scene = new Scene(grid, 1000, 600);
+    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+    return scene;
+  }
 
   public static void main(String[] args) {
     launch(args);
